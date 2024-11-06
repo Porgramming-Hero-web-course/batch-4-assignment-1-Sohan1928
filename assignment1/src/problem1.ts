327487ed456358578bab216db3904a56684838de
Sohan1928 <sohanurrahman1928@gmail.com>
2024-11-07 00:31:07 +0600

    problem task 01 is done

--- assignment1/src/problem1.ts
+++ assignment1/src/problem1.ts
@@ -1,1 +1,7 @@
-console.log("something is testing....");
+const arr: number[] = [1, 10, 2, 3, 20, 17, 4, 5];
+
+const sumArray: number = arr.reduce((accumulator, currentValue): number => {
+  return accumulator + currentValue;
+});
+
+// console.log(sumArray);
