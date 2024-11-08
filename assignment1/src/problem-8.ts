af4d2e2a7a6927cb452b876b72f90a82bd7aea7f
Sohan1928 <sohanurrahman1928@gmail.com>
2024-11-08 21:34:04 +0600

    problem 8 something edited

--- assignment1/src/problem-8.ts
+++ assignment1/src/problem-8.ts
@@ -1,9 +1,10 @@
 {
   //
-  function validateKeys(obj: object, keys: string[]): boolean {
+  function validateKeys<T extends object>(obj: T, keys: (keyof T)[]): boolean {
     return keys.every((key) => key in obj);
   }
 
+  // Sample Input:
   const person = { name: "Alice", age: 25, email: "alice@example.com" };
 
   //
